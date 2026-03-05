6acab05e89d3a78cae10747e90c10a62cc32a5b7
BDDOMAIN\christina.monrika <christina.monrika@bcadigital.co.id>
2026-03-05 14:49:26 +0700

    commit stream

---
+++ src/main/java/com/training/stream/stream.java
@@ -0,0 +1,21 @@
+package com.training.stream;
+
+import java.util.List;
+
+public class stream {
+
+    public static void main(String[] args) {
+        
+        List<Integer> transaction = List.of(500, 1200, 300, 2000, 1500, 700, 1800);
+
+        Integer integer = transaction.stream()
+        .filter(transactions -> transactions > 1000)
+        .distinct()
+        .map(trx -> trx + 50)
+        .max(Integer::compare)
+        .orElse(0);
+
+        System.out.println("hasil : " + integer);
+    }
+    
+}
